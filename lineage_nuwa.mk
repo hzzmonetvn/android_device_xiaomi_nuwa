3bf19287c9515462112b7e37f148bcd7e24b7352
HzzMonet <hzzmonet7@gmail.com>
2026-03-06 10:00:39 +0700

    Niggaos

--- lineage_nuwa.mk
+++ lineage_nuwa.mk
@@ -14,7 +14,7 @@ $(call inherit-product, device/xiaomi/nuwa/device.mk)
 # Inherit from common lineage configuration
 # $(call inherit-product, vendor/lineage/config/common_full_phone.mk) #Tày quá 
 
-PRODUCT_NAME := xiaomi_nuwa
+PRODUCT_NAME := lineage_nuwa
 PRODUCT_DEVICE := nuwa
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_BRAND := xiaomi
