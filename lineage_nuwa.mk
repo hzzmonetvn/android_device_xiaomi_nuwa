2229a405c4ddb36e2d5fd8e3e8a03762b5a3d1a5
HzzMonet <hzzmonet7@gmail.com>
2026-03-06 09:35:16 +0700

    Update lineage_nuwa.mk

--- lineage_nuwa.mk
+++ lineage_nuwa.mk
@@ -12,9 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/nuwa/device.mk)
 
 # Inherit from common lineage configuration
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# $(call inherit-product, vendor/lineage/config/common_full_phone.mk) #Tày quá 
 
-PRODUCT_NAME := lineage_nuwa
+PRODUCT_NAME := xiaomi_nuwa
 PRODUCT_DEVICE := nuwa
 PRODUCT_MANUFACTURER := xiaomi
 PRODUCT_BRAND := xiaomi
